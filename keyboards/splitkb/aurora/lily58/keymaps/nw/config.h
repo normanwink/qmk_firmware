701321310a62ebad9c550c4a57a6f94f4c84da9c
Norman Wink <mail@normanwink.com>
2025-09-02 17:03:01 +0200

    rm outdated files

---
+++ keyboards/splitkb/aurora/lily58/keymaps/nw/config.h
@@ -0,0 +1,30 @@
+/* Copyright 2022 splitkb.com <support@splitkb.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#define RGB_DI_PIN D3            // adjust if your PCB uses another pin
+#define DRIVER_LED_TOTAL 58      // total LEDs on both halves
+#define RGB_MATRIX_SPLIT         // split keyboard
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 180
+
+// Default: yellow-orange splash from pressed key
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SPLASH
+#define RGB_MATRIX_DEFAULT_HUE 28       // ~yellow-orange
+#define RGB_MATRIX_DEFAULT_SAT 255
+#define RGB_MATRIX_DEFAULT_VAL 200
+#define RGB_MATRIX_DEFAULT_SPD 20       // lower = longer effect
+
